d0204883ccf60e7ce2cfc39e0347d70dcbdee344
toracoslabs <toracoslabs@gmail.com>
2025-09-16 00:00:44 +0200

    .

--- data/mods/toracoslabs/formats-data.ts
+++ data/mods/toracoslabs/formats-data.ts
@@ -1,3 +1,12 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
-	
+	carleto:{
+		tier: "Toracoslabs"
+	},
+	pitfazu:{
+		tier: "Toracoslabs"
+	},
+	carlis:{
+		tier: "Toracoslabs"
+	}  
+
 };
